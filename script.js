eb2a700a0245d8bd09d1e1a3cd8c98228fcbdac0
Jaydeep Ingle <inglejaydeep@gmail.com>
2017-02-28 22:35:36 -0500

    Try Libraries to download pdf

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 new DroneDeploy({version: 1}).then(function(api){
   console.log('DroneDeploy Api: ', api);
+	html2canvas(document.getElementById('sample'), {
+	    useCORS: true,
+	    onrendered: function (canvas) {
+	        var data = canvas.toDataURL();
+	        var docDefinition = {
+	            content: [{
+	                image: data,
+	                width: 500,
+	            }]
+	        };
+	        pdfMake.createPdf(docDefinition).download("Map.pdf");
+	    }
+	});
 });
+
+
+
